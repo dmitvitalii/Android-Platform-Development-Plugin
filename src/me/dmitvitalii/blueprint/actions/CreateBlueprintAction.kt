dd3175808fc73040fc45f2d15c575d6a2fe857b5
Vitalii Dmitriev <dmit.vitalii@gmail.com>
2018-02-18 20:46:17 +0300

    Add a create file dialog.
    [ISSUES]
     1. Error: "This template did not produce a Java class or an interface";
     2. No templates added yet.

--- src/me/dmitvitalii/blueprint/actions/CreateBlueprintAction.kt
+++ src/me/dmitvitalii/blueprint/actions/CreateBlueprintAction.kt
@@ -17,24 +17,29 @@ package me.dmitvitalii.blueprint.actions
 
 import com.intellij.ide.actions.CreateFileFromTemplateAction
 import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFile
 import me.dmitvitalii.blueprint.lang.psi.BlueprintFileType
 
 /**
  * TODO: if we create non-default constructor with BlueprintFileType instance passed,
  * TODO: we won't be able to add it as an <action> to plugin.xml. Is there a way to do this?
+ * TODO: OR find a way to use DI to avoid x.INSTANCE.x everywhere.
+ *
  * @author Vitalii Dmitriev
  */
 class CreateBlueprintAction : DumbAware, CreateFileFromTemplateAction(
     BlueprintFileType.INSTANCE.name, BlueprintFileType.INSTANCE.description, BlueprintFileType.INSTANCE.icon
 ) {
-  override fun getActionName(dir: PsiDirectory, p1: String?, p2: String?): String {
-    TODO("not implemented")
-  }
+
+  override fun getActionName(dir: PsiDirectory, newName: String, templateName: String) = BlueprintFileType.INSTANCE.name
 
   override fun buildDialog(project: Project, dir: PsiDirectory, builder: CreateFileFromTemplateDialog.Builder) {
-    TODO("not implemented")
+    val fileType = BlueprintFileType.INSTANCE
+    builder.setTitle(fileType.name).addKind("Empty file", fileType.icon, fileType.name)
   }
 }
