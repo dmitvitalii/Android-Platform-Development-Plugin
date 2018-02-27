75aa3e73fdae04208039791af08b2a52cdac95df
Vitalii Dmitriev <Vitaly.Dmitriev@harman.com>
2018-02-27 18:23:59 +0300

    Implement a dummy completion.
    For now, BlueprintCompletion works everywhere and shows only
    module types, read from the file in from a resources directory.
    
    [ISSUES]
    1. Need to make it smart: inside module we should have completion
       for module fields (name, srcs, flags etc).
    2. Need to remove a file or to make a file only a failsafe option:
       we should read all types from the blueprint system, which are
       actually registered there.

--- src/me/dmitvitalii/blueprint/BlueprintCompletion.kt
+++ src/me/dmitvitalii/blueprint/BlueprintCompletion.kt
@@ -1,20 +1,39 @@
+/*
+ * Copyright (c) 2018 Vitalii Dmitriev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package me.dmitvitalii.blueprint
 
 import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.util.ProcessingContext
 import me.dmitvitalii.blueprint.lang.BlueprintLanguage
 import me.dmitvitalii.blueprint.lang.psi.BlueprintType
+import java.io.File
 
 class BlueprintCompletion : CompletionContributor() {
   init {
     extend(
         CompletionType.BASIC,
         // TODO: remove singleton usage!
-        PlatformPatterns.psiElement(BlueprintType.MODULE /* fixme Won't work that way.*/).withLanguage(BlueprintLanguage.INSTANCE),
+        PlatformPatterns.psiElement(BlueprintType.IDENTIFIER).withLanguage(BlueprintLanguage.INSTANCE),
         object : CompletionProvider<CompletionParameters>() {
           override fun addCompletions(params: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
-            TODO("Implement")
+            File(this::class.java.getResource("/module_names").toURI())
+                .findLines()
+                .forEach { line -> result.addElement(LookupElementBuilder.create(line)) }
           }
         }
     )
